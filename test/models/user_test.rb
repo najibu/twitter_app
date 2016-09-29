53ddde3d4fcca85f09afa3daddc268a3d3512cb8
najibu <ns.najibu@gmail.com>
2016-09-29 16:39:14 +0100

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,4 +75,32 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    najibu = users(:najibu)
+    archer  = users(:archer)
+    assert_not najibu.following?(archer)
+    najibu.follow(archer)
+    assert najibu.following?(archer)
+    assert archer.followers.include?(najibu)
+    najibu.unfollow(archer)
+    assert_not najibu.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    najibu = users(:najibu)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert najibu.feed.include?(post_following)
+    end
+    # Posts from self
+    najibu.microposts.each do |post_self|
+      assert najibu.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not najibu.feed.include?(post_unfollowed)
+    end
+  end
 end
